7cb4c8620a365ccd00c3517bbaf250c1c207fe87
orn-chfotm <chfotm@gmail.com>
2025-10-28 23:15:39 +0900

    2025-10-28 UI 구현 및 기존 코드 백업

---
+++ section03/legacy_src/app/book/[id]/page.tsx
@@ -0,0 +1,17 @@
+import ClientComponent from "@/components/client-component";
+
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+  return (
+    <div>
+      book/[id] page : {id}
+      <ClientComponent>
+        <></>
+      </ClientComponent>
+    </div>
+  );
+}
